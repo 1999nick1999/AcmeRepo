36eec3c5e8115ad9aa949a194f196f4a5d149d83
Nick van Lierop <rucklaame@gmail.com>
2024-02-29 14:12:48 +0100

    finished chapter 15

--- auth.config.ts
+++ auth.config.ts
@@ -8,6 +8,8 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      console.log(`State of isLoggedIn = ${isLoggedIn}`)
+      console.log(`State of isOnDashboard = ${isOnDashboard}`)
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
